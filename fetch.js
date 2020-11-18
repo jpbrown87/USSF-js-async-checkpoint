70003dd0d07b7e360e63f3f87e492907cc274f87
jpbrown87 <jpbrown87@gmail.com>
2020-11-18 13:42:08 -0600

    Updated code

--- fetch.js
+++ fetch.js
@@ -4,8 +4,10 @@ var fetch = require('node-fetch')
 let pokemon = 'butterfree'
 
 function fetchpokemon() {
-fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`) 
 .then(response => response.json())
-.then(json => (json.types.map(element => element.type.name)) 
-.then(data => console.log(pokemon + ": " + json.join(", ")))
+.then(json => (json.types.map(element => element.type.name)))
+.then(data => console.log(pokemon + ": " + data.join(", ")))
 }
+
+fetchpokemon()
